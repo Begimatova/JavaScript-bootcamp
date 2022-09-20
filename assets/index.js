5546a555cd5e43f2c4fc915844bfa960ad7912a3
Begimatova <aidana112599@gmail.com>
2022-09-20 18:31:05 +0600

    added js home task

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+let admin;
+let name = "john";
+admin=name;
+alert(admin);
+
+// const OUR_PLANET;
+// let userName;
+
+// `const birthday = '18.04.1982'; answer: yes
+
+// const age = someCode(birthday);` answer: no
