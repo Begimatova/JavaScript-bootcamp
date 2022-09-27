c2ceefcd15f82f202f68a57aea3b5a295336e680
Begimatova <aidana112599@gmail.com>
2022-09-27 15:58:34 +0600

    added home task

---
+++ index.js
@@ -0,0 +1,46 @@
+
+// 1) Напишите функцию которая принимает аргумент и возвращает тип данных
+
+// function dataType(a){
+// return typeof a
+// }
+ 
+// console.log(dataType(25))
+
+
+
+
+
+// 2) Напишите функцию которая принимает число и возвращает сообщение четное или не четное число
+ 
+// function evenOrOdd(a){
+//  return a%2 === 0 ? 'even': 'odd'
+// }
+
+// console.log(evenOrOdd(113))
+
+
+
+
+
+
+// 3) Напишите функцию которая ищет в объекте  значение и вернет это значение
+// 4) Напишите функцию которая ищет в объекте  ключ и вернет этот ключ
+
+// const girl = {
+//     name: 'Annie',
+//     age: 18,
+// }
+
+// function findValue(person){
+//     const newPerson = Object.assign({},person)
+//     return Object.values(newPerson)
+// }
+
+// function findKey(person){
+//     const newPerson = Object.assign({},person)
+//     return Object.keys(newPerson)
+// }
+
+// console.log(findValue(girl))
+// console.log(findKey(girl))
