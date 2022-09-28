8a703acf5aef09b67683fd19459bcb5547f2000a
Begimatova <aidana112599@gmail.com>
2022-09-28 21:35:16 +0600

    added task

---
+++ task.js
@@ -0,0 +1,214 @@
+// task-1
+
+// const arr1 = [1,2,3,4,5]
+// for (let i=0; i<arr1.length; i++){
+//     console.log(arr1[i])
+// }
+
+
+
+
+
+
+
+//task-2
+
+// const arr2 = [2, 3, 4, 5]
+// let count=1
+// for(let i=0; i<arr2.length; i++){
+//     count = count*arr2[i]
+// }
+// console.log(count)
+
+
+
+
+
+
+
+// task-3
+
+// const obj = {
+// 	'Минск': 'Беларусь',
+// 	'Москва': 'Россия',
+// 	'Киев': 'Украина'
+// };
+// for ( key in obj) {
+//     alert( key + " - это " + obj[key] );
+//   }
+
+
+
+
+
+
+
+
+//task-4
+// for(let i=11; i<=33; i++){
+//     console.log(i)
+// }
+
+
+
+
+
+
+
+
+//task-5
+// const obj = {
+//  'Коля': 200,
+//  'Вася': 300,
+//  'Петя': 400,
+// }
+// for ( key in obj) {
+//          alert( key + " - зарплата  " + obj[key] + " долларов.");
+//       }
+
+
+
+
+
+
+
+//task-6
+
+// num = 0
+// for (let n = 1000; n > 50; n--) {
+
+//     n /= 2;
+//         alert( n );
+//         num++
+//     }
+// alert("количество итераций: " + num)
+
+
+
+
+
+
+
+//task=7
+// const num=10
+// num===10? console.log('верно'):console.log('неверно')
+
+
+
+
+
+
+
+
+//task-8
+
+// min=prompt('enter min:  ')
+// if(min>=0 && min<=15){
+//     alert('первая четверть')
+// }
+// else if(min>15 && min<=30){
+//     alert('вторая четверть')
+// }
+// else if(min>30 && min<=45){
+//     alert('третья четверть')
+// }
+// else if(min>45 && min<=59){
+//     alert('третья четверть')
+// }
+// else {
+//     alert('неккоректный ввод')
+// }
+
+
+
+
+
+
+
+//task-9
+
+// lang=prompt('enter ru or en: ')
+// const arr1 = ['Понедельник','Вторник','Среда', 'Четверг','Пятница','Суббота','Воскресенье']
+// const arr2 = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+// if(lang==='ru'){
+//     arr=arr1
+// }
+// else if(lang==='en'){
+//    arr=arr2
+// }
+// else{
+//     console.log('incorrect input!')
+// }
+
+// console.log(arr)
+
+
+
+
+
+
+//task-9(2)
+
+// lang = prompt('enter ru or en: ')
+// const arr1 = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+// const arr2 = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+// let arr
+// switch (lang) {
+//     case 'ru':
+//         arr = arr1
+//         break;
+//     case 'en':
+//         arr = arr2
+//         break;
+// }
+//     console.log(arr)
+
+
+
+
+//task-9(3)
+// const array = {
+//     ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+//     en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+// }
+
+
+
+
+
+
+
+
+//task-10
+
+// let a=+prompt('enter a: ')
+// a===0? alert('Верно'):alert('неверно')
+
+//task-11
+// a=+prompt('enter a: ')
+// a>0? alert('Верно'):alert('неверно')
+
+// task-12
+// a=+prompt('enter a: ')
+// a<0? alert('Верно'):alert('неверно')
+
+// task-13
+// a=+prompt('enter a: ')
+// a>=0? alert('Верно'):alert('неверно')
+
+// task-14
+// a=+prompt('enter a: ')
+// a<=0? alert('Верно'):alert('неверно')
+
+// task-15
+// a=+prompt('enter a: ')
+// a!==0? alert('Верно'):alert('неверно')
+
+//task-16
+// a=prompt('enter a: ')
+// a==='test'? alert('Верно'):alert('неверно')
+
+
+//task-17
+// a=+prompt('enter a: ')
+// a===1? alert('Верно'):alert('неверно')
